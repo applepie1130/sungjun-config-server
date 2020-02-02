f1921b1e1fb6ac6b079aac6d78399ff34f29d296
SungJun Kim <arduous1130@gmail.com>
2020-02-02 14:07:14 +0900

    no message

---
+++ src/test/java/sungunconfigserver/SungjunConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sungunconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SungjunConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
